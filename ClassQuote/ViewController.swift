e09f6940caa854e5150b2a24b27bc0ed1cd452d6
t8lv4 <morgan@t8lv4.fr>
2018-10-06 15:44:50 +0200

    Initial Commit

---
+++ ClassQuote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClassQuote
+//
+//  Created by Morgan on 06/10/2018.
+//  Copyright © 2018 Morgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
